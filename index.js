522ae5c3ee97c6169b5e89577594bd28aeed17ad
nikolayhotim1 <hotim_nikolay@mail.ru>
2021-09-08 23:07:40 +0300

    refactoring: change var on let and add comments

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 'usestrict';
 function addSongs() {
-    var song1 = document.getElementById('song1');
-    var song2 = document.getElementById('song2');
-    var song3 = document.getElementById('song3');
-    
+    let song1 = document.getElementById('song1'); // <li></li>
+    let song2 = document.getElementById('song2'); // <li></li>
+    let song3 = document.getElementById('song3'); // <li></li>
+
     song1.innerHTML = 'Blue Suede Strings, by Elvis Pagely';
     song2.innerHTML = 'Great Objects on Fire, by Jerry JSON Lewis';
     song3.innerHTML = 'I Code the Line, by Johnny JavaScript';
 }
 
 window.onload = addSongs;
+/*
+<li>Blue Suede Strings, by Elvis Pagely</li>
+<li>Great Objects on Fire, by Jerry JSON Lewis</li>
+<li>I Code the Line, by Johnny JavaScript</li>
+*/
